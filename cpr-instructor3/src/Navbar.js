ca2eff7b1409cbc68b7293e04fcd7a1acb9b1238
Doyle Jones <doylej1616@gmail.com>
2023-04-29 10:32:56 -0400

    added course gallery and switch component

--- cpr-instructor3/src/Navbar.js
+++ cpr-instructor3/src/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+
 function Navbar() {
   return (
     <nav>
       <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
+        <li><a href="/CourseGallery">CPR Course</a></li>
         <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
